6063a4ce8829fc570a161ad28b0d3a73aaeed6fb
Rogerio Chaves <rogerio.chaves@booking.com>
2019-08-05 10:05:07 +0200

    Some Devel::Peek outputs have an _

--- lib/Test/Tdd/Generator.pm
+++ lib/Test/Tdd/Generator.pm
@@ -173,7 +173,7 @@ sub expand_globals {
 
 
 sub load_input {
-	open(STDERR, "|-", 'perl -pe "s/^\s*[A-Z]+ = .*\n//g"');
+	open(STDERR, "|-", 'perl -pe "s/^\s*[A-Z_]+ = .*\n//g"');
 	my $VAR1;
 	eval read_file(@_) or die $@;
 
